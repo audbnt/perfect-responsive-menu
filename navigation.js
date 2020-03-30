b52b5597c39ab20a3438513cfdb0c0bb112cfe7c
Audrey BONNET <audrey.bonnet.45@outlook.com>
2020-03-30 13:35:17 +0200

    ajout class noscroll quand menu ouvert

--- navigation.js
+++ navigation.js
@@ -5,4 +5,8 @@ toggleMenu.addEventListener('click', function() {
 	const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
 	toggleMenu.setAttribute('aria-expanded', !open);
 	menu.hidden = !menu.hidden;
+	// document.body.dataset.burger = !open;
+	let body = document.body;
+	body.classList.toggle("noscroll");
 });
+
